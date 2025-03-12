267a2b63721df7ea648f26bf0c558e766e338f6e
«Aleksei <Aleksmjr97@gmail.com>
2025-03-12 23:08:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// с большой буквы класс - значит КОНСТРУКТОР БАТЬ
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+  this.getElement = function (text) {
+    let element;
+    // если стартвиф (начинается с ) точки
+    if (this.selector.startsWith('.')) {
+      element = document.createElement('div'); //создаем див
+      element.classList.add(this.selector.slice(1)); // элементу добавляется класс и слайсом убираем точку
+    } // если начинается с решетки
+    else if (this.selector.startsWith('#')) {
+      element = document.createElement('p'); // создали параграф
+      element.id = this.selector.slice(1);
+    } else {
+      return;
+    }
+
+    // Применяем стили с помощью cssText
+    element.style.cssText = `
+      height: ${this.height};
+      width: ${this.width};
+      background-color: ${this.bg};
+      font-size: ${this.fontSize};
+    `;
+    // Вставляем текст в элемент
+    element.innerText = text;
+
+    // добавляем элемент на страницу
+    document.body.appendChild(element);
+    return element;
+  };
+}
+// создаем новый обьект
+const blockElement = new DomElement('.block', 100, 100, 'lightblue', 24);
+const paragraphElement = new DomElement('#btn', 50, 300, 'yellow', 24);
+
+// создаем элементы на странице
+blockElement.getElement('Создал короче блок с классом');
+paragraphElement.getElement('Создал короче блок с ID');
