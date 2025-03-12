976d1bc394b705891aa6dfa18c2bb5911f571f46
«Aleksei <Aleksmjr97@gmail.com>
2025-03-13 00:15:18 +0300

    add square

--- main.js
+++ main.js
@@ -41,3 +41,52 @@ const paragraphElement = new DomElement('#btn', 100, 500, 'yellow', 24);
 // создаем элементы на странице
 blockElement.getElement('Создал короче блок с классом');
 paragraphElement.getElement('Создал короче блок с ID');
+// дотягиваемся еще раз до нашего конструктора и создаем квадратики
+document.addEventListener('DOMContentLoaded', () => {
+  // Создаем квадрат 100x100px
+  const square = new DomElement('.square', 100, 100, 'green');
+  const squareElement = square.getElement('Леша Маскаев пытается убежать');
+
+  // Устанавливаем дополнительные стили (позицию и начальное положение)
+  squareElement.style.position = 'absolute';
+  squareElement.style.left = '600px';
+  squareElement.style.top = '100px';
+
+  let posX = 600;
+  let posY = 100;
+  const step = 20; //шаг, перенес из свитчкейса
+  let squareSize = 100; // размер квадрата для того, чтобы не уйти за пределы
+
+  // событие по кнопке (пусть шагает себе )
+
+  document.addEventListener('keydown', (event) => {
+    const maxWidth = window.innerWidth - squareSize;
+    const maxHeight = window.innerHeight - squareSize;
+
+    let reachedBorder = false; // Флаг (прикол для Семы)
+
+    switch (event.key) {
+      case 'ArrowUp':
+        if (posY > 0) posY -= step;
+        else reachedBorder = true;
+        break;
+      case 'ArrowDown':
+        if (posY < maxHeight) posY += step;
+        else reachedBorder = true;
+        break;
+      case 'ArrowLeft':
+        if (posX > 0) posX -= step;
+        else reachedBorder = true;
+        break;
+      case 'ArrowRight':
+        if (posX < maxWidth) posX += step;
+        else reachedBorder = true;
+        break;
+    }
+    if (reachedBorder) {
+      alert('CЬ*БАТЬ ПЫТАЕШЬСЯ?');
+    }
+    squareElement.style.left = posX + 'px';
+    squareElement.style.top = posY + 'px';
+  });
+});
